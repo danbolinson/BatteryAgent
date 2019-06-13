72cfe903cfb57b203c3c6eb4359d30f0c33fdb61
danbolinson <dbolinson@gmail.com>
2019-06-12 23:56:35 -0400

    Defined the Agents class.

--- batterydispatch/agent/policies.py
+++ batterydispatch/agent/policies.py
@@ -1,4 +1,9 @@
 from ..environment.helpers import last_under, first_over
+import operator
+import numpy as np
+
+def _get_max_dict_val(d):
+    return max(d.items(), key=operator.itemgetter(1))[0]
 
 def hard_limit(state, actions, period, demand_limit_dict):
     target = demand_limit_dict['target_demand']
@@ -21,3 +26,25 @@ def hard_limit(state, actions, period, demand_limit_dict):
         action = 0
 
     return action
+
+def greedy_epsilon(state, actions, period, args):
+    lr = args['learning_rate']
+    s_a_values = args['state_action_values']
+
+
+    if np.random.random() < lr:
+        action = np.random.choice(actions.options)
+    else:
+        try:
+            action = _get_max_dict_val(s_a_values[state.as_tuple()])
+        except KeyError:
+            action = 0
+
+    return action
+
+def choose_randomly(state, actions, period, args={}):
+    return np.random.choice(actions.options)
+
+def do_nothing(state, actions, period, args={}):
+    return 0
+
