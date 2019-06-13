46ff006f6420edb5b12643e8c3ed568da5381f57
danbolinson <dbolinson@gmail.com>
2019-06-12 23:58:48 -0400

    Made the .resources call dependent on package location.

--- batterydispatch/app.py
+++ batterydispatch/app.py
@@ -4,24 +4,29 @@ from pathlib import Path
 import xml.etree.ElementTree
 from batterydispatch.environment.System import System
 from batterydispatch.environment.Battery import Battery
-from batterydispatch.agent.policies import hard_limit
+from batterydispatch.agent.agents import ThresholdAgent, MonteCarloAgent
+
+import os, sys
+sys.path.append(os.path.join(os.path.dirname(__file__), "lib"))
 
 # Point a Path object at the GreenButton data
-floc = Path("./resources/")
+fdir = Path(os.path.dirname(__file__))
+floc = "resources"
 fname = "pge_electric_interval_data_2011-03-06_to_2012-04-06 A10S Med Business Large Usage.xml"
+fpath = fdir / floc / fname
 #fname = "pge_electric_interval_data_2011-03-06_to_2012-04-06 A10S Med Business Heavy Usage.xml"
 
 # Set any policy parameters and choose a policy
-policy = hard_limit
 TARGET_DEMAND = 8000
-policy_args = {'target_demand': TARGET_DEMAND}
+agent = ThresholdAgent(TARGET_DEMAND)
+agent = MonteCarloAgent()
 
 # Set up the system
-system = System(floc / fname)
+system = System(fpath)
 system.bus.add_battery(Battery(capacity=10000, power=2000))
 system.set_actions(1, 0.2)
 system.fit_states()
-system.set_policy(policy, policy_args)
+system.set_agent(agent)
 system.initialize_A()
 
 # Run the first month and capture the grid_flow output
