1a12891f4f5a15cd13aec638608ab89f0c9caa81
danbolinson <dbolinson@gmail.com>
2019-08-14 01:58:19 -0700

    Build calculation of original episodic reward into the history function to accomodate multiple days.

--- batterydispatch/agent/functions/run_episode.py
+++ batterydispatch/agent/functions/run_episode.py
@@ -42,14 +42,18 @@ def run_episodes(env, agent, eps = 0, history = [], default_reward=None, random_
         done = False
         ran_once = True
 
+        orig_reward = -1*(env.tariff.calculate_demand_charge(env.grid_flow, 'load') +
+                        env.tariff.calculate_energy_charge(env.grid_flow, 'load') * 30)
+
         print(
-            f"Current reward of {int(reward)} / {int(default_reward)}, {new_demand} / {orig_demand}, patience={agent.patience_counter}")
+            f"Current reward of {int(reward)} / {int(orig_reward)}, {new_demand} / {orig_demand}, patience={agent.patience_counter}")
+
 
         if run_type == "to_convergence":
             over = converged
         elif run_type == "once":
             over = ran_once
         # converged = agent.check_policy_convergence(False)
-        history.append((eps, reward, new_demand, default_reward, orig_demand))
+        history.append((eps, reward, new_demand, orig_reward, orig_demand))
 
     return reward
